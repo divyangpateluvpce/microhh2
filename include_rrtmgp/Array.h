147f31c7652bf6d95d5f9c3b5dfbb469ffb210d5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2018-12-05 21:58:39 +0100

    Implemented c and fortran indexing

--- include_rrtmgp/Array.h
+++ include_rrtmgp/Array.h
@@ -9,19 +9,29 @@ template<int N>
 inline std::array<int, N> calc_strides(const std::array<int, N>& dims)
 {
     std::array<int, N> strides;
-    strides[0] = 1;
-    for(int i=1; i<N; ++i)
-        strides[i] = strides[i-1]*dims[i-1];
+    strides[N-1] = 1;
+    for (int i=N-2; i>=0; --i)
+        strides[i] = strides[i+1]*dims[i+1];
 
     return strides;
 }
 
 template<int N>
-inline int dot(const std::array<int, N>& left, const std::array<int, N>& right)
+inline int c_index(const std::array<int, N>& index, const std::array<int, N>& stride)
 {
     int sum = 0;
     for (int i=0; i<N; ++i)
-        sum += left[i]*right[i];
+        sum += index[i]*stride[i];
+
+    return sum;
+}
+
+template<int N>
+inline int fortran_index(const std::array<int, N>& index, const std::array<int, N>& stride)
+{
+    int sum = 0;
+    for (int i=0; i<N; ++i)
+        sum += (index[i]-1)*stride[N-1-i];
 
     return sum;
 }
@@ -59,15 +69,29 @@ struct Array
         this->data = data;
     }
 
+    // C++-style indexing with []
+    inline T& operator[](const std::array<int, N>& indices)
+    {
+        const int index = c_index<N>(indices, strides);
+        return data[index];
+    }
+
+    inline T operator[](const std::array<int, N>& index) const
+    {
+        const int i = c_index<N>(index, strides);
+        return data[i];
+    }
+
+    // Fortran-style indexing with []
     inline T& operator()(const std::array<int, N>& indices)
     {
-        const int index = dot<N>(indices, strides);
+        const int index = fortran_index<N>(indices, strides);
         return data[index];
     }
 
     inline T operator()(const std::array<int, N>& index) const
     {
-        const int i = dot<N>(index, strides);
+        const int i = fortran_index<N>(index, strides);
         return data[i];
     }
 
